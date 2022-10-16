7c37bebc536582bd2e2166cc3bba6511b40950cc
TheoDeveloper <97988283+TheoDeveloping@users.noreply.github.com>
2022-10-16 09:21:40 -0700

    forgot add

---
+++ source/android/MobileControls.hx
@@ -0,0 +1,82 @@
+package android;
+
+import flixel.FlxG;
+import flixel.group.FlxSpriteGroup;
+import android.FlxVirtualPad;
+import android.Hitbox;
+import android.Save;
+
+class MobileControls extends FlxSpriteGroup
+{
+	public var mode:ControlsGroup = HITBOX;
+
+	public var _hitbox:Hitbox;
+	public var _virtualPad:FlxVirtualPad;
+
+	var _config:ControlMode;
+
+	public function new() 
+	{
+		super();
+		
+		_config = new ControlMode();
+
+		// load control mode num from ControlMode.hx
+		mode = getModeFromNumber(_config.getcontrolmode());
+		trace (_config.getcontrolmode());
+
+		switch (mode)
+		{
+			case VIRTUALPAD_RIGHT:
+				initVirtualPad(0);
+			case VIRTUALPAD_LEFT:
+				initVirtualPad(1);
+			case VIRTUALPAD_CUSTOM:
+				initVirtualPad(2);
+			case HITBOX:
+				_hitbox = new Hitbox();
+				add(_hitbox);
+			case KEYBOARD:
+		}
+	}
+
+	function initVirtualPad(vpadMode:Int) 
+	{
+		switch (vpadMode)
+		{
+			case 1:
+				_virtualPad = new FlxVirtualPad(FULL, NONE);
+			case 2:
+				_virtualPad = new FlxVirtualPad(FULL, NONE);
+				_virtualPad = _config.loadcustom(_virtualPad);
+			default: // 0
+				_virtualPad = new FlxVirtualPad(RIGHT_FULL, NONE);
+		}
+		
+		_virtualPad.alpha = 0.75;
+		add(_virtualPad);	
+	}
+
+
+	public static function getModeFromNumber(modeNum:Int):ControlsGroup {
+		return switch (modeNum)
+		{
+			case 0: VIRTUALPAD_RIGHT;
+			case 1: VIRTUALPAD_LEFT;
+			case 2: KEYBOARD;
+			case 3: VIRTUALPAD_CUSTOM;
+			case 4:	HITBOX;
+
+			default: VIRTUALPAD_RIGHT;
+
+		}
+	}
+}
+
+enum ControlsGroup {
+	VIRTUALPAD_RIGHT;
+	VIRTUALPAD_LEFT;
+	KEYBOARD;
+	VIRTUALPAD_CUSTOM;
+	HITBOX;
+}
